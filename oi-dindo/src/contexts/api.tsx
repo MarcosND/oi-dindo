76545bd2a86c08cd6a7b22fdc136ed569ba1b3d9
Joao Almeida <jvitor@Joaos-MacBook-Air-2.local>
2023-04-11 12:52:06 -0300

    integrating the api

--- oi-dindo/src/contexts/api.tsx
+++ oi-dindo/src/contexts/api.tsx
@@ -1,7 +1,10 @@
 import React, { createContext, useContext, useState } from 'react';
 import { useGet } from '../hooks/useGet';
+import { Pet } from '../models/pet';
+import { Ong } from '../models/ong';
 
-const api_url = "localhost:4000";
+const host = "http://192.168.15.106:1337"
+const api_url = `${host}/api`;
 
 type ApiContextType = {
   apiData: string[];
@@ -30,13 +33,24 @@ export const useApi = () => {
   if (!context) throw new Error('useApi must be used within an ApiProvider')
   const { apiData, setApiData } = context;
 
-  const getPets = useGet<Pet[]>(`${api_url}/pets`);
-  const getOngs = useGet<Ong[]>(`${api_url}/ongs`);
+  // Gets lists of all entities
+  const getPets = useGet<Pet[]>(`${api_url}/pets`, {isList: true, populate: true});
+  const getOngs = useGet<Ong[]>(`${api_url}/ongs`, {isList: true, populate: true});
+
+  // Finds specific entity with id
+  const findPhoto = (url: string) => `${host+url}`
+  const findPet = useGet<Pet>(`${api_url}/pet`, {isList: false, populate: true});
+  const findOng = useGet<Ong>(`${api_url}/ong`, {isList: false, populate: true});
 
   return {
-    get: {
+    list: {
         pets: getPets,
         ongs: getOngs,
+    },
+    find: {
+        photo: findPhoto,
+        pet: findPet,
+        ong: findOng,
     }
   }
 }
--- oi-dindo/src/pages/Home/index.tsx
+++ oi-dindo/src/pages/Home/index.tsx
@@ -1,41 +1,59 @@
 import { Title } from "@mui/icons-material";
-import { Typography, Stack } from "@mui/material";
-import { FunctionComponent } from "react";
+import { Typography, Stack, Button, Card, CardContent, Avatar, Box } from "@mui/material";
+import { FunctionComponent, useState } from "react";
+import { useApi } from "../../contexts/api";
+import { Pet } from "../../models/pet";
 
 interface HomeProps {
     
 }
  
 const Home: FunctionComponent<HomeProps> = () => {
+    const api = useApi();
+
+    const [pets, setPets] = useState<Pet[]>();
+    
     return (
       <Stack direction="column" spacing={7}>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
-        <Typography variant="h1" component="h2">
-            Home
-        </Typography>
+        <Button
+            onClick={async () => {
+                setPets(await api.list.pets())
+            }}
+        >
+            FETCH    
+        </Button>
+        {
+            pets?.map(pet => {
+                const photoUrl =api.find.photo(pet.photo.data.attributes.url)
+                return <Card key={pet.id}>
+                    <CardContent>
+                    <Box
+                        component="img"
+                        sx={{
+                            height: 233,
+                            width: 350,
+                            maxHeight: { xs: 233, md: 167 },
+                            maxWidth: { xs: 350, md: 250 },
+                        }}
+                        alt={pet.name}
+                        src={photoUrl}
+                    />
+                    <Typography variant="h3">
+                        {pet.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {pet.description}
+                    </Typography>
+                    <Typography variant="h5">
+                        Peso: {pet.weight}kg
+                    </Typography>
+                    <Typography variant="h5">
+                        Idade: {pet.age} anos
+                    </Typography>
+                    </CardContent>
+                </Card>
+            })
+        }
       </Stack>
     );
 }
