c480c0ca221ab64ea9a0cf2ea12e3065808d0806
Felipo <gabriel.felipo2010@gmail.com>
2023-04-16 10:19:56 -0300

    Continuando a tela de pet

--- oi-dindo/src/pages/Pet/index.tsx
+++ oi-dindo/src/pages/Pet/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, CardActions, CardContent, Container, Button, Typography, Box, LinearProgress, Stack } from "@mui/material";
+import { Avatar, Card, CardActions, CardContent, Container, Button, Typography, Box, LinearProgress, Stack, Hidden } from "@mui/material";
 import { FunctionComponent } from "react";
 import avatar from "../../images/Alfredo.png"
 
@@ -12,10 +12,11 @@ const PetPage: FunctionComponent<PetPageProps> = () => {
             <Avatar  
             src={avatar} 
             variant="square"
-            sx={{ width: '100vw', height: '120vw' }}
+            sx={{ width: '90vw', height: '120vw' }}
             />
 
             <Card elevation={0}>
+                <Stack spacing={"0px"}>
                 <CardContent>
                     <Box sx={{padding: "16px", border: "1px solid blue", borderRadius: "14px"}}>
                         <Typography sx={{paddingBottom:"8px"}} variant="h5" component="div">
@@ -30,13 +31,13 @@ const PetPage: FunctionComponent<PetPageProps> = () => {
 
                 <CardActions>
                     <Button 
-                    sx={{borderRadius:2, minHeight: 40}} 
-                    variant="contained">Apadrinhar
+                    sx={{borderRadius:2, minHeight: 40, left: "50%", margin:0, msTransform: "translateX(-50%)", transform: "translateX(-50%)"}} 
+                    variant="contained">
+                        Apadrinhar
                     </Button>
                 </CardActions>
 
                 <CardContent>
-                    <Box sx={{padding: "16px", border: "1px solid blue", borderRadius: "14px"}}>
                         <Typography sx={{paddingBottom:"4px"}} variant="h5" component="div">
                             Sobre o pet:
                         </Typography>
@@ -46,71 +47,77 @@ const PetPage: FunctionComponent<PetPageProps> = () => {
                         <Typography sx={{paddingTop:"12px"}} variant="body1">
                         #carinhoso #curioso #pelocurto #brincalhão #obediente #sociável
                         </Typography>
-                    </Box>
                 </CardContent>
-
+                
                 <CardActions>
                     <Stack direction="row" spacing={"11px"}>
                         <Avatar  
                             src={avatar} 
-                            sx={{}}
                         />
                         <Button 
-                            sx={{borderRadius:2, minHeight: 40}} 
+                            sx={{borderRadius:2}} 
                             variant="contained">Apadrinhar
                         </Button>
                     </Stack>
                 </CardActions>
 
                 <CardContent>
-                    <Box sx={{padding: "16px", border: "1px solid blue", borderRadius: "14px"}}>
+                    <Box sx={{padding: "16px", border: "1px solid blue", borderRadius: "14px", width:"100vw"}}>
                             <Typography sx={{paddingBottom:"4px"}} variant="h5" component="div">
                                 Perguntas
                             </Typography>
 
                             <Box sx={{overflow: "auto", whiteSpace: "nowrap"}}>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    Home
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    News
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    Contact
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    teste1
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    teste2
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    teste3
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    teste4
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    teste4
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    teste4
-                                </Typography>
-                                <Typography sx={{padding:"8px", display: "inline-block", textAlign: "center"}} variant="body1">
-                                    teste4
-                                </Typography>
+                                <Stack direction="row" spacing={"11px"}>
+                                    <Box sx={{backgroundColor: "blue", display: "inline-block", minWidth:230, whiteSpace: "normal"}}>
+                                        <Typography sx={{padding:"8px", textAlign: "left"}} variant="h6">
+                                            Pergunta
+                                        </Typography>
+                                        <Typography sx={{paddingLeft:"8px", paddingRight:"8px", paddingBottom:"8px",wordBreak: "normal", overflow: "hidden"}} variant="body1" align="left">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+                                        </Typography>
+                                    </Box>
+                                    <Box sx={{backgroundColor: "blue", display: "inline-block", minWidth:230, whiteSpace: "normal"}}>
+                                        <Typography sx={{padding:"8px", textAlign: "left"}} variant="h6">
+                                            Pergunta
+                                        </Typography>
+                                        <Typography sx={{paddingLeft:"8px", paddingRight:"8px", paddingBottom:"8px",wordBreak: "normal", overflow: "hidden"}} variant="body1" align="left">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+                                        </Typography>
+                                    </Box>
+                                    <Box sx={{backgroundColor: "blue", display: "inline-block", minWidth:230, whiteSpace: "normal"}}>
+                                        <Typography sx={{padding:"8px", textAlign: "left"}} variant="h6">
+                                            Pergunta
+                                        </Typography>
+                                        <Typography sx={{paddingLeft:"8px", paddingRight:"8px", paddingBottom:"8px",wordBreak: "normal", overflow: "hidden"}} variant="body1" align="left">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+                                        </Typography>
+                                    </Box>
+                                    <Box sx={{backgroundColor: "blue", display: "inline-block", minWidth:230, whiteSpace: "normal"}}>
+                                        <Typography sx={{padding:"8px", textAlign: "left"}} variant="h6">
+                                            Pergunta
+                                        </Typography>
+                                        <Typography sx={{paddingLeft:"8px", paddingRight:"8px", paddingBottom:"8px",wordBreak: "normal", overflow: "hidden"}} variant="body1" align="left">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+                                        </Typography>
+                                    </Box>
+                                    <Box sx={{backgroundColor:"clear", display: "inline-block", minWidth:100}}></Box>
+                                </Stack>
                             </Box>
                     </Box>
                 </CardContent>
-
-                <CardActions>
+                </Stack>
                     <br />
                     <br />
                     <br />
                     <br />
                     <br />
                     <br />
-                </CardActions>
+                
             </Card>
         </Container>
     );
